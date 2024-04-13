b0a2c2ac11fdd738e180c5af34a5329d8359630c
ELLa123 <313097973@qq.com>
2024-04-14 00:48:29 +0800

    格式化

--- src/Functions.php
+++ src/Functions.php
@@ -1,30 +1,34 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 
 namespace Ella123\HyperfCaptcha;
 
-
 use Hyperf\Stringable\Str;
+
 use function Hyperf\Support\make;
 
 /**
  * 创建图片验证码
- * @param  string|null  $key
- * @return array
  */
-function captcha_create(string $key = null): array
+function captcha_create(?string $key = null): array
 {
     return [
-        'key' => !$key && $key = Str::random(),
+        'key' => empty($key) && $key = Str::random(),
         'img' => make(CaptchaInterface::class)->create($key),
     ];
 }
 
 /**
  * 验证图片验证码
- * @param  string  $code
- * @param  string  $key
- * @return mixed
  */
 function captcha_verify(string $code, string $key): bool
 {
