6e5e26da6a0ff9b9b4e2d66321dce77ea91e6c8c
ELLa123 <313097973@qq.com>
2024-04-14 01:13:42 +0800

    修改：目录结构

--- src/CaptchaService.php
+++ src/CaptchaService.php
@@ -170,7 +170,7 @@ class CaptchaService implements CaptchaInterface
         // 验证码字体随机颜色
         $this->color = @imagecolorallocate($this->im, mt_rand(1, 150), mt_rand(1, 150), mt_rand(1, 150));
         // 验证码使用随机字体
-        $ttfPath = __DIR__ . '/../../assets/' . ($this->useZh ? 'zhttfs' : 'ttfs') . '/';
+        $ttfPath = __DIR__ . '/assets/' . ($this->useZh ? 'zhttfs' : 'ttfs') . '/';
 
         if (empty($this->fontttf)) {
             $dir = dir($ttfPath);
